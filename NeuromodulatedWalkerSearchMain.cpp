cad9197e992fa3fca25c83e7358d615e049e3691
jasoyode <jasoyode@indiana.edu>
2017-10-05 14:37:12 -0400

    added a number of tests and added a sinusoidal option (afte debugging it)

--- NeuromodulatedWalkerSearchMain.cpp
+++ NeuromodulatedWalkerSearchMain.cpp
@@ -5,6 +5,9 @@
 #include "random.h"
 #include "CTRNN.h"
 #include "LeggedAgent.h"
+#include "assert.h"
+
+#define PI 3.141592653589793238
 
 // Following example in text
 // Evolving walking: The anatomy of an evolutionary search (2004)
@@ -22,7 +25,6 @@ const double maxTimingConstant = 10.0;
 //Neurons of neurons
 const int networkSize = 3;
 
-
 // ****************************************************
 // Added parameters for extrinsic neuromodulation
 // ****************************************************
@@ -34,29 +36,40 @@ const int neuronParameterCount = 3;
 //receptor = 0 means no modulation happens (if min=max=0 then no modulation)
 //receptor = 1 means full effect
 const int maxReceptor = 1;
-const int minReceptor = 0;
-
+const int minReceptor = -1;
 
 //Modulation signal oscillates between these two bounds
-//The boundaries of the modulation signal 0.5 increases by 50%, i.e.  1.5 multiplier
+//The boundaries of the modulation signal; 0.5 increases by 50%, i.e.  1.5 multiplier
 const double maxModulation = 0.5;
 const double minModulation = -0.5;
-
-//Number of oscillations of modulating signal to send
-//Assuming 8 maximum steps, to coincide with steps, should be approximately factors of 8
-const int externalModulationPeriods = 2;
-const double modulationStepSize =  (maxModulation - minModulation ) *  ( externalModulationPeriods / (RunDuration / StepSize) )  ;
 
 
 //When turned off, disables modulation and calls normal Step function
 const bool modulationEnabled = true;
+//Linear oscillation
+//Number of oscillations of modulating signal to send
+//Assuming 8 maximum steps, to coincide with steps, should be approximately factors of 8
+//uses sin function instead of linear increment when true
+const bool sinusoidalOscillation = true;
+const int externalModulationPeriods = 8;
+
+//calculated based on other constants
+const double modulationStepSize =  2 * (maxModulation - minModulation ) *  ( externalModulationPeriods / (RunDuration / StepSize) )  ;
+//if set to -1 then use continuous values
+const int discreteLevelsOfModulation = 3;
+
 
 
 //translate value to discrete set of values
+//    level -1 = return continuous value
 //    levels 2  =  {min, max}
 //    levels 3 =  {min, (max+min)/2, max}
 //
 double TranslateDoubleToDiscreteValues( double d, int levels ) {
+    //-1 means use continuous values)
+    if (levels == -1 ) {
+        return d;
+    }
 
     //cout << "d:" << d << " mapped to ";
     double range = maxReceptor - minReceptor;
@@ -72,10 +85,106 @@ double TranslateDoubleToDiscreteValues( double d, int levels ) {
     }
     //cout <<  maxReceptor << endl;
     return maxReceptor;
-    
-       
 }
 
+
+void runTests(bool showTests) {
+    
+    //for linear increment oscillation
+    double modVel= modulationStepSize;
+    //for instantenous modulation level
+    double modulationLevel = 0;
+    //how often to show the current modulation level
+    int displayFreq = 100;
+    int count=0;
+    int hitTopCount=0;
+    int hitBotCount=0;
+    
+    for (double time = 0; time < RunDuration; time += StepSize) {
+        //use global modulation enabled to determine step to update    
+        //oscillate back and forth between bounds
+        if (modulationLevel >= maxModulation ) {
+            if ( showTests ) {
+                cout << "Hit max going DOWN! @" << time << endl;
+            }
+            modVel = -modulationStepSize;
+            hitTopCount++;
+        } else if ( modulationLevel < minModulation ) {
+            modVel = modulationStepSize;
+            if ( showTests ) {
+                cout << "Hit min going UP! @" << time << endl;
+            }
+            hitBotCount++;
+        } 
+        modulationLevel += modVel;
+        count++;
+        if (showTests &&  count % displayFreq == 1) {
+            cout << "time: " << time << "  modulation: " << modulationLevel;
+            //calculate sin and then scale to modulation bounds
+            //used this to debug a silly bug I kept getting
+            //double sinCalc = sin(  time / (RunDuration / externalModulationPeriods) * 2 * PI );
+            //double center = (maxModulation+minModulation) / 2 ;
+            //cout << "center: " << center << endl;
+            //double scale = (maxModulation-minModulation )/2;
+            //cout << "scale: " << scale << endl;
+            //sinCalc =     center + scale  * sinCalc ;
+            
+            double sinCalc = (maxModulation+minModulation) / 2 + (maxModulation-minModulation )/2 * sin(  time / (RunDuration / externalModulationPeriods) * 2 * PI );
+            
+            cout << "  vs. sin calculated2: " << sinCalc << endl;
+            cout << "    " << ( modulationLevel - sinCalc ) << endl;
+            assert(  modulationLevel - sinCalc  < 0.25 );
+        }
+            
+    } 
+    
+    //verify that the correct number of oscillations occurs when
+    assert( hitTopCount == externalModulationPeriods && externalModulationPeriods == hitBotCount );
+    cout << "externalModulationPeriods is working properly" << endl;
+    
+    if ( showTests ) {
+        cout << "hitTopCount: "<< hitTopCount << " ==  " << externalModulationPeriods << endl;
+    }
+    
+    double a = MapSearchParameter( -0.9, minReceptor, maxReceptor);
+    double b = MapSearchParameter( -0.1, minReceptor, maxReceptor);
+    double c = MapSearchParameter( 0.1 , minReceptor, maxReceptor);
+    double d = MapSearchParameter( 0.9 , minReceptor, maxReceptor);
+    
+    int levels=2;
+    
+    assert( TranslateDoubleToDiscreteValues(a, levels) == minReceptor );
+    assert( TranslateDoubleToDiscreteValues(b, levels) == minReceptor );
+    assert( TranslateDoubleToDiscreteValues(c, levels) == maxReceptor );
+    assert( TranslateDoubleToDiscreteValues(d, levels) == maxReceptor );
+    
+    cout << "TranslateDoubleToDiscreteValues with 2 values working" << endl;
+    
+    
+    if ( showTests ) {
+        levels=2;
+        cout << "---------------------------------------------" <<endl;
+        cout << "translate -0.9 with 2 levels: " << TranslateDoubleToDiscreteValues(a, levels) << endl;
+        cout << "translate -0.1 with 2 levels: " << TranslateDoubleToDiscreteValues(b, levels) << endl;
+        cout << "translate 0.1 with 2 levels: " << TranslateDoubleToDiscreteValues(c, levels) << endl;
+        cout << "translate 0.9 with 2 levels: " << TranslateDoubleToDiscreteValues(d, levels) << endl;
+        
+        levels=3;
+        cout << "---------------------------------------------" <<endl;
+        cout << "translate -0.9 with 3 levels: " << TranslateDoubleToDiscreteValues(a, levels) << endl;
+        cout << "translate -0.1 with 3 levels: " << TranslateDoubleToDiscreteValues(b, levels) << endl;
+        cout << "translate 0.1 with 3 levels: " << TranslateDoubleToDiscreteValues(c, levels) << endl;
+        cout << "translate 0.9 with 3 levels: " << TranslateDoubleToDiscreteValues(d, levels) << endl;
+        
+        levels=4;
+        cout << "---------------------------------------------" <<endl;
+        cout << "translate -0.9 with 4 levels: " << TranslateDoubleToDiscreteValues(a, levels) << endl;
+        cout << "translate -0.1 with 4 levels: " << TranslateDoubleToDiscreteValues(b, levels) << endl;
+        cout << "translate 0.1 with 4 levels: " << TranslateDoubleToDiscreteValues(c, levels) << endl;
+        cout << "translate 0.9 with 4 levels: " << TranslateDoubleToDiscreteValues(d, levels) << endl;
+    }
+    return;
+}
 
 
 //Evaluate the performance of externally modulated CTRNN controllers with walking task
@@ -106,13 +215,10 @@ double Evaluate(TVector<double> &v, RandomState &rs)
        //this is the double value that regulates how much neurons are impacted by the modulation signal
        paramCount++;
        
-       
-       //double receptorStrength = discretizeReceptor(   MapSearchParameter(v[paramCount], minReceptor, maxReceptor)       );
-       
-       double receptorStrength = TranslateDoubleToDiscreteValues(   MapSearchParameter(v[paramCount], minReceptor, maxReceptor), 2 );
+       double receptorStrength = TranslateDoubleToDiscreteValues(   MapSearchParameter(v[paramCount], minReceptor, maxReceptor), discreteLevelsOfModulation );
        c.SetNeuronReceptor(i, receptorStrength ) ;
        
-       //continuous
+       //continuous - original
        //c.SetNeuronReceptor(i, MapSearchParameter(v[paramCount], minReceptor, maxReceptor) ) ;
     }
 
@@ -148,14 +254,23 @@ double Evaluate(TVector<double> &v, RandomState &rs)
     
       //use global modulatioEnabled to determine whether to use modulation step or not
       if (modulationEnabled) {
-          //oscillate modulationLevel back and forth between bounds
-          if (modulationLevel >= maxModulation ) {
-              modVel = -modulationStepSize;
-          } else if ( modulationLevel < minModulation ) {
-              modVel = modulationStepSize;
-          } 
-          modulationLevel += modVel;        
-          //I use method chaining to pass this all the way down to the CTRNN where I define a special ModulatedStep function
+          
+          if ( sinusoidalOscillation ) {
+              //calculate sin and then scale to modulation bounds
+              modulationLevel = (maxModulation+minModulation) / 2 + (maxModulation-minModulation )/2 * sin(  time / (RunDuration / externalModulationPeriods) * 2 * PI );
+              
+              
+          } else {
+              //oscillate modulationLevel back and forth between bounds
+              if (modulationLevel >= maxModulation ) {
+                  modVel = -modulationStepSize;
+              } else if ( modulationLevel < minModulation ) {
+                  modVel = modulationStepSize;
+              } 
+              modulationLevel += modVel;        
+          }
+
+          //I use method chaining to pass this all the way down to the CTRNN where I define a special ModulatedStep function          
           Insect.ModulatedStep(StepSize, modulationLevel);
         
         } else {
@@ -178,6 +293,15 @@ int MyTerminationFunction(int Generation,  double BestPerf,  double AvgPerf,  do
 
 // The main program
 int main (int argc, const char* argv[]) {
+
+  //verify settings are reasonable - true shows info
+  cout << "Running tests..." << endl;
+  runTests( false );
+  //only run tests
+  //return -1;
+  
+  
+
   TSearch s(  networkSize*neuronParameterCount + networkSize*networkSize   );
     
   // Configure the search
@@ -187,7 +311,7 @@ int main (int argc, const char* argv[]) {
   s.SetSelectionMode(RANK_BASED);
   s.SetReproductionMode(HILL_CLIMBING);
   s.SetPopulationSize(1000);
-  s.SetMaxGenerations(500);
+  s.SetMaxGenerations(2500);
   
   //start with 0.5 (per paper results)
   s.SetMutationVariance(0.5);
@@ -214,62 +338,15 @@ int main (int argc, const char* argv[]) {
     
   // Display the best individual found
   cout << s.BestIndividual() << endl;
-
+  
+  cout << "------------------------------------------------------------------" << endl;
+  cout << "Best performing agent has the following neuron receptor strengths:" << endl;
+  
+  int startingReceptor = 2 * networkSize;
+  for (int i=1; i <= networkSize; i++ ) {
+      double val = TranslateDoubleToDiscreteValues(   MapSearchParameter(s.BestIndividual()[startingReceptor+i], minReceptor, maxReceptor), discreteLevelsOfModulation );
+      cout << "Neuron " << i << " has receptor strength: " << val << endl;
+  }
   return 0;
-}
-
-
-// The second main program to test impact of modulation on performance of Walker from file
-// May be broken as the cpg.ns will not include receptors, so cannot use receptors
-int alternate_test_main(int argc, char* argv[])
-{
-    LeggedAgent Insect;
-
-
-    // Load the CTRNN into the agent
-    char fname[] = "cpg.ns";
-    ifstream ifs;
-    ifs.open(fname);
-    if (!ifs) {
-        cerr << "File not found: " << fname << endl;
-        exit(EXIT_FAILURE);
-    }
-    ifs >> Insect.NervousSystem;
-
-    // Run the agent
-    SetRandomSeed( 1 ); //RandomSeed);
-    Insect.Reset(0, 0, 0);
-    
-    double modVel=0;
-    double modulationLevel=0;
-
-    for (double time = 0; time < RunDuration; time += StepSize) {
-            
-        //use global modulation enabled to determine step to update    
-        if (modulationEnabled) {
-            //oscillate back and forth between bounds
-            if (modulationLevel >= maxModulation ) {
-                modVel = -modulationStepSize;
-            } else if ( modulationLevel < minModulation ) {
-                modVel = modulationStepSize;
-            } 
-            modulationLevel += modVel;        
-            Insect.ModulatedStep(StepSize, modulationLevel);
-        
-        } else {
-            //regular step
-            Insect.Step(StepSize);
-        
-        }
-        //cout << Insect.Leg.JointX << " " << Insect.Leg.JointY << " ";
-        //cout << Insect.Leg.FootX << " " << Insect.Leg.FootY << " ";
-        //cout << Insect.Leg.FootState << endl;
-    }
-
-    // Display the fitness
-    cout << "Average velocity = " << Insect.cx/RunDuration << endl;
-
-    // Finished
-    return 0;
 }
 
