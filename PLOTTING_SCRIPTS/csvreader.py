49e1e9323b32152ed2082a52bcdcdc8fd3fd76e4
jasoyode <jasoyode@indiana.edu>
2017-10-08 01:50:59 -0400

    decided to make PLOTS folder for storing plotted results

--- PLOTTING_SCRIPTS/csvreader.py
+++ PLOTTING_SCRIPTS/csvreader.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import csv
 import glob
@@ -8,38 +9,18 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
+#location for DATA from experiments and PLOTS
+DATA="../DATA"
+PLOTS="../PLOTS"
+
 
 if len(sys.argv) < 2:
-  print(  "Usage: "+ sys.argv[0]+ " experiment_directory_name (inside ../DATA/" )
+  print(  "Usage: "+ sys.argv[0]+ " experiment_directory_name (inside {}".format( DATA ) )
   quit()
-
-
-
 
 experiment_directory=sys.argv[1]
 
-#print( experiment_directory )
-#quit()
 
-
-#  Launch script todo
-#    1. save config file to directory
-#    2. add test of best evolved walker and record values to file (along with modulation signal!)
-#    3. 
-
-
-#   Plotting todo
-#DONE     1. CRAWL PASSED IN DIRECTORY rather than iterate through
-#DONE     2. Make plot of all the individual runs in one plot
-#DONE     3. Make aggregate plot with shaded regions are error space (1 STD)
-#     4. Make comparison plots of all the different runs that are similar
-#DEPENDS ON ABOVE     5. Make plot of the agent speed over time, overlayed with neuron activations and modulation signal  
-
-
-#  Plot speed of agent relative to the modulation signal
-#  Plot 
-
-#with open('../DATA/2017-10-06-standard_3neuron/fitness_and_receptors.txt') as csvfile:
 
 #each element is a collection of all the generations in 1 run
 gen_all = []
@@ -50,7 +31,6 @@ fit_all = []
 fit_by_gen = []
 
 
-
 #START FIRST IMAGE FOR PLOTTING
 plt.figure(0)
 
@@ -59,7 +39,7 @@ plt.figure(0)
 #for i in range(0, generations):
 #  fit_by_gen.append( [] )
 
-seed_files =  glob.glob(  '../DATA/{}/seed_*.txt'.format( experiment_directory  ) ) 
+seed_files =  glob.glob(  '{}/{}/seed_*.txt'.format( DATA, experiment_directory  ) ) 
 
 for seed_file in seed_files:
   gen = []
@@ -125,13 +105,16 @@ for g in gen: #range(0, len(fit_by_gen) ):
   #print( "gen: {}  mean: {}   std: {}".format( g, gen_means[g], gen_errors[g] ) )
 
 
+#make sure folder exists
+os.system( "mkdir -p {}/{}".format( PLOTS, experiment_directory ) )
+
 
 plt.plot(gen, fit)
 plt.xlabel('Generation')
 plt.ylabel('Best Fitness')
 plt.title( experiment_directory + '\nBest Fitness of Individual Experiment Runs')
 plt.grid(True)
-plt.savefig("../DATA/{}/individual_runs.png".format( experiment_directory ) )
+plt.savefig("{0}/{1}/individual_runs_{1}.png".format(PLOTS,  experiment_directory ) )
 
 
 
@@ -151,7 +134,7 @@ plt.xlabel('Generation')
 plt.ylabel('Best Mean Fitness')
 plt.title( experiment_directory + '\nMean Best Fitness by Generation with Standard Error')
 plt.grid(True)
-plt.savefig("../DATA/{}/mean_runs_with_error.png".format( experiment_directory ) )
+plt.savefig("{0}/{1}/mean_runs_with_error_{1}.png".format(PLOTS, experiment_directory ) )
 
 
 
