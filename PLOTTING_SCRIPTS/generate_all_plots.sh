9fc8241743b9c65d0b5d0c8a717e8b11ffa56f1b
jasoyode <jasoyode@indiana.edu>
2017-10-12 12:11:52 -0400

    fixed bug added new parameter format

--- PLOTTING_SCRIPTS/generate_all_plots.sh
+++ PLOTTING_SCRIPTS/generate_all_plots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILTER="2017-10-10-BEST_exp_size-3_M-std_recep-POS-OFF-NEG_exp-long_10_modsig-lin-1p";
+FILTER="2017-1";
 
 for dir in $( ls ../DATA/ | grep "$FILTER" ); do
  python csvreader.py $dir
