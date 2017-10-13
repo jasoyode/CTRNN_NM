7fc653770b246c233bdd0a92a7013948a69631a8
jasoyode <jasoyode@indiana.edu>
2017-10-13 13:33:14 -0400

    fixed path

--- scripts/post_processing/post_process_over_ssh.sh
+++ scripts/post_processing/post_process_over_ssh.sh
@@ -10,9 +10,9 @@ if [[ $HOSTNAME == *"karst"* ]]; then
     echo "on Karst"
     SUB_DATA=$( echo "$1" | sed "s/.*DATA\///" )
     
-    rsync -av --progress $1 jasoyode@silo.cs.indiana.edu:~/github_jasoyode/CTRNN_NM/DATA/$SUB_DATA
+    rsync -av --progress $1 jasoyode@silo.cs.indiana.edu:/scratch/jasoyode/github_jasoyode/CTRNN_NM/DATA/$SUB_DATA
     
-    ssh jasoyode@silo.cs.indiana.edu "cd /u/jasoyode/github_jasoyode/CTRNN_NM/scripts/post_processing/ && ./post_process.sh ../../DATA/$SUB_DATA" 
+    ssh jasoyode@silo.cs.indiana.edu "cd /scratch/jasoyode/github_jasoyode/CTRNN_NM/scripts/post_processing/ && ./post_process.sh ../../DATA/$SUB_DATA" 
     echo "PLEASE RUN the following command on silo: "
     echo "./tar_and_store.sh ../../DATA/$SUB_DATA ./../PLOTS/$SUB_DATA" 
     
