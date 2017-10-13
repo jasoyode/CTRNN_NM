5fa163589e04316ebdcf4fd1dbe392e9615e151f
jasoyode <jasoyode@indiana.edu>
2017-10-13 14:20:26 -0400

    debugging

--- scripts/post_processing/post_process_over_ssh.sh
+++ scripts/post_processing/post_process_over_ssh.sh
@@ -11,7 +11,7 @@ if [[ $HOSTNAME == *"karst"* ]]; then
     SUB_DATA=$( echo "$1" | sed "s/.*DATA\///" )
     
     rsync -av --progress $1 jasoyode@silo.cs.indiana.edu:/scratch/jasoyode/github_jasoyode/CTRNN_NM/DATA/$SUB_DATA
-    
+    echo "SUBDATA = $SUBDATA"
     ssh jasoyode@silo.cs.indiana.edu "cd /scratch/jasoyode/github_jasoyode/CTRNN_NM/scripts/post_processing/ && ./post_process.sh ../../DATA/$SUB_DATA" 
     echo "PLEASE RUN the following command on silo: "
     echo "./tar_and_store.sh ../../DATA/$SUB_DATA ../../PLOTS/$SUB_DATA" 
