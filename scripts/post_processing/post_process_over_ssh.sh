1f19000de0a7347f5d369c7dacb2b87605d46aff
jasoyode <jasoyode@indiana.edu>
2017-10-13 13:29:03 -0400

    print command dont run

--- scripts/post_processing/post_process_over_ssh.sh
+++ scripts/post_processing/post_process_over_ssh.sh
@@ -13,8 +13,8 @@ if [[ $HOSTNAME == *"karst"* ]]; then
     rsync -av --progress $1 jasoyode@silo.cs.indiana.edu:~/github_jasoyode/CTRNN_NM/DATA/$SUB_DATA
     
     ssh jasoyode@silo.cs.indiana.edu "cd /u/jasoyode/github_jasoyode/CTRNN_NM/scripts/post_processing/ && ./post_process.sh ../../DATA/$SUB_DATA" 
-    
-    ssh jasoyode@silo.cs.indiana.edu "cd /u/jasoyode/github_jasoyode/CTRNN_NM/scripts/post_processing/ && ./tar_and_store.sh ../../DATA/$SUB_DATA ./../PLOTS/$SUB_DATA" 
+    echo "PLEASE RUN the following command on silo: "
+    echo "./tar_and_store.sh ../../DATA/$SUB_DATA ./../PLOTS/$SUB_DATA" 
     
 else
     echo "not on Karst!"
