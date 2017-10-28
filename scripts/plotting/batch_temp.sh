89f28cb0c87ba2098f3a0ed8eb44c40aea4a1c29
jasoyode <jasoyode@indiana.edu>
2017-10-27 23:22:21 -0400

    adding 100 run jobs

--- scripts/plotting/batch_temp.sh
+++ scripts/plotting/batch_temp.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#DIR="../../DATA/GENS-2000/JOB_size-3_sim-long10run-2000gen_signal-SINE-1p_M-mod1-ON"
-DIR="../../DATA/JASON/JOB_size-3_sim-long10run_signal-SINE-1p_M-standard"
+DIR="../../DATA/GENS-2000/JOB_size-3_sim-long10run-2000gen_signal-SINE-1p_M-mod1-ON"
+#DIR="../../DATA/JASON/JOB_size-3_sim-long10run_signal-SINE-1p_M-standard"
 
-#python csvreader.py $DIR const_mod.csv
-#python csvreader $DIR const_mod_NEG.csv
-#python csvreader.py $DIR const_mod_POS.csv
-#python csvreader.py $DIR periods_1-5.csv
-#python csvreader.py $DIR periods.csv
-#python csvreader.py $DIR const_mod_extremes.csv
+python csvreader.py $DIR CSV/const_mod.csv &
+python csvreader.py $DIR CSV/const_mod_NEG.csv & 
+python csvreader.py $DIR CSV/const_mod_POS.csv & 
+python csvreader.py $DIR CSV/periods_1-5.csv & 
+python csvreader.py $DIR CSV/periods.csv &
+python csvreader.py $DIR CSV/const_mod_extremes.csv
 
 
 
 #python csvreader.py $DIR test_standard_plus_minus.csv
-python csvreader.py $DIR test_standard_pos.csv
+#python csvreader.py $DIR test_standard_pos.csv
 #python csvreader.py $DIR test_standard_pos_neg.csv
 #python csvreader.py $DIR test_standard_neg.csv
